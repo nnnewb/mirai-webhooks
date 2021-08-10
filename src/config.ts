3da94f39053a2b3b440ad65527f01e3897c0b8f8
weak_ptr <weak_ptr@outlook.com>
2021-08-10 09:37:12 +0800

    refactor(config): 对configprovider少许改进，减少三元表达式和or运算符

--- src/config.ts
+++ src/config.ts
@@ -12,43 +12,60 @@ export interface ConfigProvider {
   notify_groups: number[];
   notify_users: number[];
 
-  friend_secret: string;
+  friend_secret: string | null;
   template_dir: string;
 }
 
 export class ConfigError extends Error {}
 
 export class EnvironmentConfigProvider implements ConfigProvider {
+  get_env_number(name: string): number | null {
+    const num = parseInt(process.env[name] as string);
+    return isNaN(num) ? null : num;
+  }
+
+  get_env_number_with_default(name: string, defaultValue: number): number {
+    return this.get_env_number(name) || defaultValue;
+  }
+
+  get_env_string(name: string): string | null {
+    return (process.env[name] as string) || null;
+  }
+
+  get_env_string_with_default(name: string, defaultValue: string): string {
+    return this.get_env_string(name) || defaultValue;
+  }
+
   public get uin(): number {
-    if (!process.env.MIRAI_WEBHOOKS_UIN) {
+    const uin = this.get_env_number('MIRAI_WEBHOOKS_UIN');
+    if (uin === null) {
       throw new ConfigError('environment variable MIRAI_WEBHOOKS_UIN not set');
     }
-    return parseInt(process.env.MIRAI_WEBHOOKS_UIN || '');
+    return uin;
   }
 
   public get pwd(): string | null {
-    return process.env.MIRAI_WEBHOOKS_PWD || null;
+    return this.get_env_string('MIRAI_WEBHOOKS_PWD');
   }
 
   public get platform(): number | null {
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    return process.env.MIRAI_WEBHOOKS_PLATFORM ? parseInt(process.env.MIRAI_WEBHOOKS_PLATFORM!) : null;
+    return this.get_env_number('MIRAI_WEBHOOKS_PLATFORM');
   }
 
   public get data_dir(): string {
-    return process.env.MIRAI_WEBHOOKS_DATA_DIR || path.resolve(process.cwd(), 'oicq-data');
+    return this.get_env_string_with_default('MIRAI_WEBHOOKS_DATA_DIR', path.resolve(process.cwd(), 'oicq-data'));
   }
 
   public get port(): number {
-    return process.env.MIRAI_WEBHOOKS_PORT ? parseInt(process.env.MIRAI_WEBHOOKS_PORT) : 6543;
+    return this.get_env_number_with_default('MIRAI_WEBHOOKS_PORT', 6543);
   }
 
   public get hostname(): string {
-    return process.env.MIRAI_WEBHOOKS_HOSTNAME ? process.env.MIRAI_WEBHOOKS_HOSTNAME : '0.0.0.0';
+    return this.get_env_string_with_default('MIRAI_WEBHOOKS_HOSTNAME', '127.0.0.1');
   }
 
   public get administrator(): number | null {
-    return process.env.MIRAI_WEBHOOKS_ADMINISTRATOR ? parseInt(process.env.MIRAI_WEBHOOKS_ADMINISTRATOR) : null; // todo
+    return this.get_env_number('MIRAI_WEBHOOKS_ADMINISTRATOR');
   }
 
   public get notify_groups(): number[] {
@@ -61,19 +78,21 @@ export class EnvironmentConfigProvider implements ConfigProvider {
   }
 
   public get notify_users(): number[] {
-    if (process.env.MIRAI_WEBHOOKS_NOTIFY_USERS) {
-      return process.env.MIRAI_WEBHOOKS_NOTIFY_USERS.split(',')
+    const env_string = this.get_env_string('MIRAI_WEBHOOKS_NOTIFY_USERS');
+    if (env_string) {
+      return env_string
+        .split(',')
         .map((value) => parseInt(value))
         .filter((r) => r);
     }
     return [];
   }
 
-  public get friend_secret(): string {
-    return process.env.MIRAI_WEBHOOKS_FRIEND_SECRET || '';
+  public get friend_secret(): string | null {
+    return this.get_env_string('MIRAI_WEBHOOKS_FRIEND_SECRET');
   }
 
   public get template_dir(): string {
-    return process.env.MIRAI_WEBHOOKS_TEMPLATE_DIR || path.resolve(__dirname, '../templates');
+    return this.get_env_string_with_default('MIRAI_WEBHOOKS_TEMPLATE_DIR', path.resolve(__dirname, '../templates'));
   }
 }
